4f6adfa8c4e409dfc31c108cd8b318748fb33cda
Diogo Monteiro <diogo322@hotmail.com>
2024-01-03 22:38:10 -0300

    First Commit

---
+++ ControlePesoElevador/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+
+        int[] pesoPessoas = { 30, 60, 95, 100, 120, 98 };
+        int soma = 0;
+        
+        for(int i = 0; i < pesoPessoas.Length; i++)
+        {
+            if (pesoPessoas[i] > 90)
+            {
+                Console.WriteLine($"Você tem {pesoPessoas[i]} kg, não podm entrar");
+                Console.WriteLine("Próximo por favor");
+                Console.WriteLine();
+                continue;
+            }
+            soma += pesoPessoas[i];
+        }
+        Console.WriteLine(soma);
+    }
+}
